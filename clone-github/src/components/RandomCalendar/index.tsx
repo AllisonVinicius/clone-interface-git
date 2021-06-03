c8831052c4b1fba943b52d68596de0efdb3f8182
AllisonVinicius <allisonvinicius63@gmail.com>
2021-06-03 13:17:32 -0400

    estilizando calendario de contribuicoes

--- clone-github/src/components/RandomCalendar/index.tsx
+++ clone-github/src/components/RandomCalendar/index.tsx
@@ -1,9 +1,43 @@
 import React from 'react';
+import Heatmap from 'react-calendar-heatmap';
+import { Container } from './styles';
+import {subYears} from 'date-fns';
 
-// import { Container } from './styles';
+type HeatmapValue = {date: Date, count: number};
+
 
 const RandomCalendar: React.FC = () => {
-  return <div />;
-}
+    const startDate = subYears(new Date(), 1);
+    const endDate = new Date();
+    const values: HeatmapValue[] = [];
+
+    values.push({date: new Date(), count: 3});
+
+  return (
+
+    <Container>
+        <div className="wrapper">
+            <Heatmap 
+                startDate={startDate}
+                endDate={endDate}
+                values={values}
+                gutterSize={3.5}
+                classForValue={(item: HeatmapValue) => {
+                    let clampedCount = 0;
+                    if(item !== null ){
+                        clampedCount = Math.max(item.count,0);
+                        clampedCount = Math.max(item.count,4);
+
+                    }
+                    return 'scale-${clampedCount}';
+                }}
+                showWeekdayLabels
+            
+            />
+        </div>
+        <span>Nao representa dados reais do git</span>
+    </Container>
+  );
+};
 
 export default RandomCalendar;
