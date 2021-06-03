12275fd346424647aede39b0d936e7df9799dffc
AllisonVinicius <allisonvinicius63@gmail.com>
2021-06-03 12:33:23 -0400

    adiconando imports

--- clone-github/src/pages/Profile/index.tsx
+++ clone-github/src/pages/Profile/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
- import { Container, Main, LeftSide, RightSide } from './styles';
+ import { Container, Main, LeftSide, RightSide,Repos} from './styles';
+
 import ProfileData from '../../components/ProfileData';
+import RepoCard from '../../components/RepoCard';
 
 
 const Profile: React.FC = () => {
@@ -22,7 +24,28 @@ const Profile: React.FC = () => {
           
           />
         </LeftSide>
-        <RightSide></RightSide>
+        <RightSide>
+          <Repos>
+            <h2>rando Repo</h2>
+            <div>
+              {[1,2,3,4,5,6].map(index => (
+                <RepoCard 
+                  key={index}
+                  username={'AllisonVinicius'}
+                  reponame={'darhboard-react'}
+                  description={'git clone'}
+                  language={index % 3 ===  0 ?'JavaScript' : 'TypeScript'}
+                  stars={5}
+                  forks={4}
+                
+                />
+
+
+              ))};
+            </div>
+
+          </Repos>
+        </RightSide>
       </Main>
     </Container>
 
