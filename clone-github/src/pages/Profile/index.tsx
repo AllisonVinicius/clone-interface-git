68ea695f6b5ea4073fc51fbd0d062a5705fe081a
AllisonVinicius <allisonvinicius63@gmail.com>
2021-06-03 08:28:42 -0400

    estruturando page profile e adicionando info que sera exibidas

--- clone-github/src/pages/Profile/index.tsx
+++ clone-github/src/pages/Profile/index.tsx
@@ -1,9 +1,32 @@
 import React from 'react';
 
-// import { Container } from './styles';
+ import { Container, Main, LeftSide, RightSide } from './styles';
+import ProfileData from '../../components/ProfileData';
+
 
 const Profile: React.FC = () => {
-  return <div />;
-}
+  return (
+    <Container>
+      <Main>
+        <LeftSide>
+          <ProfileData 
+            username={'AllisonVinicius'}
+            name={'Allison Vinicius'}
+            avatarUrl={'https://avatars.githubusercontent.com/u/39178001?v=4'}
+            followers={887}
+            following={4}
+            company={'Nenhuma'}
+            location={'Corumbá,Brazil'}
+            email={'alllisonvinicius63@gmail.com'}
+            blog={'https://www.linkedin.com/in/allison-vinicius-b73a9a147/'}
+          
+          />
+        </LeftSide>
+        <RightSide></RightSide>
+      </Main>
+    </Container>
+
+  );
+};
 
 export default Profile;
