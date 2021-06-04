ac853666c29fdf2f2ca4be6156f3d55371962b31
AllisonVinicius <allisonvinicius63@gmail.com>
2021-06-03 23:16:12 -0400

    footer

---
+++ clone-github/src/components/Footer/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+import { Container, GithubLogo, Line } from './styles';
+
+const Footer: React.FC = () => {
+  return (
+    <Container>
+        <Line />
+        <GithubLogo />
+    </Container>
+
+  )
+}
+
+export default Footer;
