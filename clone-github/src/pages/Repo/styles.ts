92782aa7ce928d60af1b5f8bc125e2a780df0014
AllisonVinicius <allisonvinicius63@gmail.com>
2021-06-03 21:04:54 -0400

    ajustando styles dos components

--- clone-github/src/pages/Repo/styles.ts
+++ clone-github/src/pages/Repo/styles.ts
@@ -1,5 +1,44 @@
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
+
+import {RiBookMarkLine,RiStarLine} from 'react-icons/ri';
+import {AiOutlineFork} from 'react-icons/ai';
+import {FaGithub} from 'react-icons/fa';
+
 
 export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: 16px;
+
+    > p {
+        font-size: 16px;
+    }
   
 `;
+export const Breakcrumb = styled.div`
+    
+
+`;
+
+const iconCss = css`
+    width: 16px;
+    height: 16px;
+    fill: var(--icon);
+    flex-shrink: 0;
+`;
+
+
+export const Stats = styled.div``;
+
+
+export const RepoIcon = styled(RiBookMarkLine)``;
+
+export const StarIcon = styled(RiStarLine)`
+    ${iconCss}
+`;
+
+export const ForkIcon = styled(AiOutlineFork)``;
+
+export const LinkButton = styled.a``;
+
+export const GithubIcon = styled(FaGithub)``;
